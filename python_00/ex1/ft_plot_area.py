02b6f1014dfc6ed0326a38016577a294e2348490
Jacinto Abad Diaz <jabad-di@student.42malaga.com>
2026-02-24 18:32:41 +0100

    inicios python

---
+++ python_00/ex1/ft_plot_area.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#                                                                             #
+#                                                          :::      ::::::::  #
+#   ft_plot_area.py                                      :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: jabad-di <jabad-di@student.42malaga.com>     +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/02/24 17:51:31 by jabad-di            #+#    #+#            #
+#   Updated: 2026/02/24 18:19:07 by jabad-di           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+def ft_plot_area():
+    length = int(input("Enter legth: "))
+    width = int(input("Enter legth; "))
+    print("plot area:", length * width)
